bd701e96d61dd495638f72af84adc9db69b66669
congtrinhmn <43317945+congtrinhmn@users.noreply.github.com>
2018-09-21 22:53:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/congtrinh/event3/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.congtrinh.event3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText edtSoa, edtSob;
+    TextView tvKetQua;
+    Button btnTong2so;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtSoa = (EditText) findViewById(R.id.edtSoa);
+        edtSob = (EditText) findViewById(R.id.edtSob);
+        tvKetQua = (TextView) findViewById(R.id.tvKetQua);
+        btnTong2so = (Button) findViewById(R.id.btnTong2so);
+
+        btnTong2so.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int a = Integer.parseInt(edtSoa.getText() + "");
+        int b = Integer.parseInt(edtSob.getText() + "");
+        tvKetQua.setText((a + b)+"");
+    }
+}
